5a8e360d21a0f80fe079fa3da69940c89909a7f5
Paul Vaneveld <paul@liters.nl>
2021-11-07 14:18:26 +0100

    WIP

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,22 @@
 <template>
   <div id="app">
     <Header></Header>
-    <main class="max-w-4xl h-6 my-0 mx-auto flex">
-      <p class="text-gray-700 mt-20	text-center my-0 mx-auto max-w-2xl">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, in expedita. Quaerat unde ad exercitationem aliquid asperiores nihil inventore voluptates quasi tempora nobis molestiae dolor iure, eligendi delectus. Natus, accusamus?</p>
+    <main class="max-w-4xl h-6 my-0 mx-auto flex flex-col">
+      <p class="text-gray-700 mt-20 mb-5	text-center my-0 mx-auto max-w-2xl">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, in expedita. Quaerat unde ad exercitationem aliquid asperiores nihil inventore voluptates quasi tempora nobis molestiae dolor iure, eligendi delectus. Natus, accusamus?</p>
+      <Article/>
     </main>
   </div>
 </template>
 
 <script>
 import Header from './components/Header.vue';
+import Article from './components/Article.vue';
 
 export default {
   name: 'App',
   components: {
-    Header
+    Header,
+    Article,
   }
 }
 </script>
---
+++ src/components/Article.vue
@@ -0,0 +1,16 @@
+<template>
+<div class="flex items-center">
+  <span class="bg-gray-600 text-white py-1 px-2 rounded mr-5">20-10-2021</span>
+  <div class="flex border-yellow-300 border-l-4 py-5 pl-10 relative">
+    <div class="w-3 h-3 bg-yellow-300 absolute -left-2 top-1/2 dot"></div>
+    <div class="max-w-lg bg-gray-100 rounded px-2 py-1">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad laudantium dolorum voluptates dignissimos et! Corporis, quos consequuntur! Quod facilis ullam placeat nostrum assumenda quia qui, a, blanditiis, voluptatibus quasi cum?</div>
+  </div>
+  
+</div>
+</template>
+
+<style scoped>
+.dot {
+  transform: translateY(-50%);
+}
+</style>
