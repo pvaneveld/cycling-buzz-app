1e780a751ec46e017171f1043eda2e324858291b
Paul Vaneveld <paul@liters.nl>
2021-11-08 09:00:52 +0100

    WIP

--- src/components/Article.vue
+++ src/components/Article.vue
@@ -3,7 +3,10 @@
   <span class="bg-gray-600 text-white py-1 px-2 rounded mr-5">20-10-2021</span>
   <div class="flex border-yellow-300 border-l-4 py-5 pl-10 relative">
     <div class="w-3 h-3 bg-yellow-300 absolute -left-2 top-1/2 dot"></div>
-    <div class="max-w-lg bg-gray-100 rounded px-2 py-1">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad laudantium dolorum voluptates dignissimos et! Corporis, quos consequuntur! Quod facilis ullam placeat nostrum assumenda quia qui, a, blanditiis, voluptatibus quasi cum?</div>
+    <a href="/" target="_blank" class="hover:underline max-w-lg bg-gray-100 rounded px-2 py-1 text-xl text-gray-700 flex items-center cursor-pointer">
+      Healine headline headline
+      <img class="w-4 h-4 ml-2" src="./../assets/002-share.png" alt="">
+    </a>
   </div>
   
 </div>
